1ab18692e993c9ec17106777488d1e7ec00156c1
Taras Slipets <taras.slipets@gmail.com>
2012-05-15 19:39:45 +0300

    semantic lab initial commit

---
+++ src/semantic/SemanticAnalyzer.java
@@ -0,0 +1,11 @@
+package semantic;
+
+import nametable.NameTableBuilder;
+
+public class SemanticAnalyzer {
+    private NameTableBuilder nameTableBuilder;
+
+    public SemanticAnalyzer(NameTableBuilder nameTableBuilder) {
+    this.nameTableBuilder = nameTableBuilder;
+    }
+}
