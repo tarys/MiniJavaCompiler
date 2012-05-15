36327cd7cc43b01d95b64d5634c89ae867faab5a
Taras Slipets <taras.slipets@gmail.com>
2012-05-15 19:47:28 +0300

    next commit

--- src/semantic/SemanticAnalyzer.java
+++ src/semantic/SemanticAnalyzer.java
@@ -4,8 +4,22 @@ import nametable.NameTableBuilder;
 
 public class SemanticAnalyzer {
     private NameTableBuilder nameTableBuilder;
+    private boolean breakFlag;
 
     public SemanticAnalyzer(NameTableBuilder nameTableBuilder) {
-    this.nameTableBuilder = nameTableBuilder;
+        this.nameTableBuilder = nameTableBuilder;
+    }
+
+
+    public void setBreakFlag(boolean breakFlag) {
+        this.breakFlag = breakFlag;
+    }
+
+    public boolean isBreakFlag() {
+        return breakFlag;
+    }
+
+    public void checkNotUsedBreak() throws SemanticException {
+        throw new SemanticException("\"break\" used but cycle not declared");
     }
 }
---
+++ src/semantic/SemanticException.java
@@ -0,0 +1,10 @@
+package semantic;
+
+public class SemanticException extends Exception {
+    public SemanticException() {
+    }
+
+    public SemanticException(String message) {
+        super(message);
+    }
+}
